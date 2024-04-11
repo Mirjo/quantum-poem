052c77f9ad39f8e668c726c1008f195722169a41
Mirco Boeckmann <mirco.boeckmann@adesso.de>
2024-04-12 01:08:37 +0200

    clean up

--- src/main.py
+++ src/main.py
@@ -1,121 +1,76 @@
-#-----Step 1: Use VideoCapture in openCV-----
+# -----Step 1: Use VideoCapture in openCV-----
 import cv2
 import dlib
-import math
 import random
-BLINK_RATIO_THRESHOLD = 5.7
 
-#-----Step 5: Getting to know blink ratio
-
-def midpoint(point1 ,point2):
-    return (point1.x + point2.x)/2,(point1.y + point2.y)/2
-
-def euclidean_distance(point1 , point2):
-    return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
-
-def get_blink_ratio(eye_points, facial_landmarks):
-    
-    #loading all the required points
-    corner_left  = (facial_landmarks.part(eye_points[0]).x, 
-                    facial_landmarks.part(eye_points[0]).y)
-    corner_right = (facial_landmarks.part(eye_points[3]).x, 
-                    facial_landmarks.part(eye_points[3]).y)
-    
-    center_top    = midpoint(facial_landmarks.part(eye_points[1]), 
-                             facial_landmarks.part(eye_points[2]))
-    center_bottom = midpoint(facial_landmarks.part(eye_points[5]), 
-                             facial_landmarks.part(eye_points[4]))
-
-    #calculating distance
-    horizontal_length = euclidean_distance(corner_left,corner_right)
-    vertical_length = euclidean_distance(center_top,center_bottom)
-
-    ratio = horizontal_length / vertical_length
-
-    return ratio
-
-line1= 'It\'s always dark'
-line2= 'In the ancient glade'
-line3= 'Across old bark'
-line4= 'The quite shade'
+line1 = "It's always dark"
+line2 = "In the ancient glade"
+line3 = "Across old bark"
+line4 = "The quite shade"
 prev_list = []
+face_counter = 0
+no_face_counter = 0
+
 
 def print_new_poem():
     global prev_list
-    poem_list = [line1,line2,line3,line4]
+    poem_list = [line1, line2, line3, line4]
     while prev_list == poem_list:
         random.shuffle(poem_list)
     prev_list = poem_list
 
-    print('\r\n')
-    print('\r\n')
-    print('A small poem')
-    print('\r\n')
+    print("\r\n")
+    print("\r\n")
+    print("A small quantum poem")
+    print("\r\n")
     for entry in poem_list:
         print(entry)
-    print('\r\n')
-    print('\r\n')
+    print("\r\n")
+    print("\r\n")
 
 
-
-#livestream from the webcam 
+# livestream from the webcam
 cap = cv2.VideoCapture(0)
 
-'''in case of a video
-cap = cv2.VideoCapture("__path_of_the_video__")'''
+"""in case of a video
+cap = cv2.VideoCapture("__path_of_the_video__")"""
 
-#name of the display window in openCV
-cv2.namedWindow('BlinkDetector')
+# name of the display window in openCV
+cv2.namedWindow("BlinkDetector")
 
-#-----Step 3: Face detection with dlib-----
+# -----Step 3: Face detection with dlib-----
 detector = dlib.get_frontal_face_detector()
-
-#-----Step 4: Detecting Eyes using landmarks in dlib-----
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-#these landmarks are based on the image above 
-left_eye_landmarks  = [36, 37, 38, 39, 40, 41]
-right_eye_landmarks = [42, 43, 44, 45, 46, 47]
 
 print_new_poem()
 
 while True:
-    #capturing frame
+    # capturing frame
     retval, frame = cap.read()
 
-    #exit the application if frame not found
+    # exit the application if frame not found
     if not retval:
         print("Can't receive frame (stream end?). Exiting ...")
-        break 
+        break
 
-    #-----Step 2: converting image to grayscale-----
+    # -----Step 2: converting image to grayscale-----
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    #-----Step 3: Face detection with dlib-----
-    #detecting faces in the frame 
-    faces,_,_ = detector.run(image = frame, upsample_num_times = 0, adjust_threshold = 0.0)
+    # -----Step 3: Face detection with dlib-----
+    # detecting faces in the frame
+    faces, _, _ = detector.run(image=frame, upsample_num_times=0, adjust_threshold=0.0)
 
-    #-----Step 4: Detecting Eyes using landmarks in dlib-----
-    for face in faces:
-        
-        landmarks = predictor(frame, face)
+    # -----Step 4: Detecting Eyes using landmarks in dlib-----
+    if len(faces) == 0:
+        face_counter = 0
+        no_face_counter += 1
+    else:
+        face_counter += 1
+        no_face_counter = 0
 
-        #-----Step 5: Calculating blink ratio for one eye-----
-        left_eye_ratio  = get_blink_ratio(left_eye_landmarks, landmarks)
-        right_eye_ratio = get_blink_ratio(right_eye_landmarks, landmarks)
-        blink_ratio     = (left_eye_ratio + right_eye_ratio) / 2
-
-        if blink_ratio > BLINK_RATIO_THRESHOLD:
-            #Blink detected! Do Something!
-            cv2.putText(frame,"BLINKING",(10,50), cv2.FONT_HERSHEY_SIMPLEX,
-            2,(255,255,255),2,cv2.LINE_AA)
-            print_new_poem()
+    if len(faces) == 0 and no_face_counter == 1:
+        print_new_poem()
 
 
-    # cv2.imshow('BlinkDetector', frame)
-    # key = cv2.waitKey(1)
-    # if key == 27:
-    #     break
-
-#releasing the VideoCapture object
+# releasing the VideoCapture object
 cap.release()
 cv2.destroyAllWindows()
